5f2983ec8a820a1202ed608bc8a975e55f52a5c5
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-19 15:38:44 +0100

    I MADE TABULAR DISPLAY AND MANY TO MANY RELATIONSHIP

--- core/catalog/admin.py
+++ core/catalog/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from .models import Category, Expense, Expense_Transaction, Users
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Expense)
-admin.site.register(Expense_Transaction)
-admin.site.register(Users)
+# admin.site.register(Expense)
+# admin.site.register(Expense_Transaction)
+# admin.site.register(Users)
+# Define the admin class
+class AuthorUser(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Users, AuthorUser)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'amount', 'date', 'user_id', 'display_category')
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(Expense_Transaction) 
+class ExpenseTransactionAdmin(admin.ModelAdmin):
+    list_filter = ('payment_type', 'trans_date')
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'expense', 'posted_amount')
+        }),
+        ('Transaction Type', {
+            'fields': ('payment_type', 'trans_date')
+        }),
+    )
+    pass
--- core/catalog/models.py
+++ core/catalog/models.py
@@ -18,13 +18,18 @@ class Expense(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=1000, help_text='Enter a brief description of the Expense')
     amount = models.DecimalField(decimal_places=2, help_text='Enter Expense Amount', max_digits=9)
-    date = models.DateField(auto_created=True, editable=False)
+    date = models.DateField(null=True, blank=True)
     def __str__(self):
         """String for representing the Model object."""
         return self.title
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('expense-detail', args=[str(self.id)])
+    def display_category(self):
+        """Create a string for the Category. This is required to display category in Admin."""
+        return ', '.join(category_id.name for category_id in self.category_id.all()[:3])
+    #give it a short name
+    display_category.short_description = 'Category'
 class Expense_Transaction(models.Model):
     """Model representing a specific Transaction of Expenses"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular Expense across whole System')
